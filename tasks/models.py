db77ca74cec0d521519164e9f9fb297d2102add6
Rodrio Erico <rodrigoerico25@gmail.com>
2022-11-29 04:02:19 -0400

    created task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    OPTIONS_CATEGORY = (
+        ('urgent', 'Urgente'),
+        ('important', 'Importante'),
+        ('when_you_have_time', 'Quando tiver tempo')
+    )
+    OPTIONS_STATUS = (
+        ('concluded', 'Concluído'),
+        ('pending', 'Pendente'),
+        ('suspended', 'Suspenso'),
+    )
+
+    description = models.CharField(max_length=400)
+    creation = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(
+        max_length=25, choices=OPTIONS_CATEGORY, default='important')
+    status = models.CharField(
+        max_length=25, choices=OPTIONS_STATUS, default='pending')
